ab97d4f0668c9a925bacc0e291d4dbfd1b0c63fd
Tyler Suehr <tylersuehr@gmail.com>
2024-11-27 17:35:54 -0500

    Core game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+#include "colors.hpp"
+#include <raylib.h>
+
+static constexpr const int windowWidth = 500;
+static constexpr const int windowHeight = 620;
+static constexpr const int fps = 120;
+
 int main(void) {
+    InitWindow(windowWidth, windowHeight, "Tetris");
+    SetTargetFPS(120);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(Colors::window_color);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
