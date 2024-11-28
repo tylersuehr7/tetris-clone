fc4bc563bc559deec6f5b3d39b3fa0dee6e20ea7
Tyler Suehr <tylersuehr@gmail.com>
2024-11-27 19:35:36 -0500

    Block colors

--- src/colors.cpp
+++ src/colors.cpp
@@ -3,3 +3,17 @@
 const Color Colors::window_color = RAYWHITE;
 
 const Color Colors::grid_color = LIGHTGRAY;
+
+const Color Colors::block_colors[] = {
+    Colors::grid_color,  // BCI_GRID
+    {26, 31, 40, 255},   // BCI_DARK_GREY
+    {47, 230, 23, 255},  // BCI_GREEN
+    {232, 18, 18, 255},  // BCI_RED
+    {226, 116, 17, 255}, // BCI_ORANGE
+    {237, 234, 4, 255},  // BCI_YELLOW
+    {166, 0, 247, 255},  // BCI_PURPLE
+    {21, 204, 209, 255}, // BCI_CYAN
+    {13, 64, 216, 255},  // BCI_BLUE
+    {59, 85,162,255},    // BCI_LIGHT_BLUE
+    {44, 44, 127,255},   // BCI_DARK_BLUE
+};
--- src/colors.hpp
+++ src/colors.hpp
@@ -2,7 +2,23 @@
 
 #include <raylib.h>
 
+typedef enum : unsigned char {
+    BCI_GRID = 0,
+    BCI_DARK_GREY,
+    BCI_GREEN,
+    BCI_RED,
+    BCI_ORANGE,
+    BCI_YELLOW,
+    BCI_PURPLE,
+    BCI_CYAN,
+    BCI_BLUE,
+    BCI_LIGHT_BLUE,
+    BCI_DARK_BLUE,
+    BCI_MAX
+} BlockColorIndex;
+
 namespace Colors {
     extern const Color window_color;
     extern const Color grid_color;
+    extern const Color block_colors[BCI_MAX];
 }
